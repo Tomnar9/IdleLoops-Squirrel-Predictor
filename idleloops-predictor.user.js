5091d7696919bc694e59db8c11d40bebc18d16dd
MakroCZ <marek.barinka@gmail.com>
2022-07-10 20:18:29 +0200

    Mercantilism rep usage

--- idleloops-predictor.user.js
+++ idleloops-predictor.user.js
@@ -646,7 +646,10 @@ const Koviko = {
           r.artifacts -= 1;
           r.favor += 1;
         }},
-        'Mercantilism': { effect: (r, k) => k.mercantilism += 100 },
+        'Mercantilism': { canStart: (input) => (input.rep >= 0), effect: (r, k) => {
+          k.mercantilism += 100;
+          r.rep--;
+        }},
         'Charm School': {},
         'Enchant Armor': { affected: ['armor', 'favor', 'enchantments'], canStart: (input) => {
           return (input.armor >= 0 && input.favor >= 0);
