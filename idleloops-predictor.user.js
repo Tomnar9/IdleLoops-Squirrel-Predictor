d83ac6bd6408fd7429af2b05cd3b2bb4202aa0e0
Tomnar9 <109023593+Tomnar9@users.noreply.github.com>
2022-09-09 11:29:05 +0200

    incremented version number

--- idleloops-predictor.user.js
+++ idleloops-predictor.user.js
@@ -2,7 +2,7 @@
 // @name         IdleLoops Predictor Makro
 // @namespace    https://github.com/MakroCZ/
 // @downloadURL  https://raw.githubusercontent.com/MakroCZ/IdleLoops-Predictor/master/idleloops-predictor.user.js
-// @version      2.2.6
+// @version      2.3.0
 // @description  Predicts the amount of resources spent and gained by each action in the action list. Valid as of IdleLoops v.85/Omsi6.
 // @author       Koviko <koviko.net@gmail.com>
 // @match        https://lloyd-delacroix.github.io/omsi-loops/
